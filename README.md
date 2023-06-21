82f7a238900eb51985c1bb16c46822a92e077d5e
Ryan Nazareth <ryankarlos@gmail.com>
2023-06-22 00:17:07 +0100

    fix pipeline run

---
+++ README.md
@@ -0,0 +1,64 @@
+
+#### Instructions on running mlflow and dvc pipeline
+
+```bash
+pip intall poetry
+```
+
+Install dependencies from poetry lock file
+
+```bash
+poetry install
+```
+
+Then run the pipeline script to track a new run.  These will be saved in
+a new folder with tracking id in runs folder.
+
+**NOTE**: Specified folder needs to be created during execution when  
+referenced for the first time. If it is created upfront, the script  
+terminates with the following error message: mlflow.exceptions.MlflowException:
+`Could not find experiment with ID 0`. This is set as `runs` folder in root of repo
+in the code
+
+```bash
+poetry run python -m mlflow_with_dvc.pipeline
+```
+
+You should see the output as below
+
+```bash
+INFO:__main__:Elasticnet model (alpha=0.500000, l1_ratio=0.500000):
+INFO:__main__:RMSE: 0.447125790081536
+INFO:__main__:MAE: 0.36150258728645435
+INFO:__main__:R2: 0.7044956342219149
+Successfully registered model 'ElasticnetWineModel'.
+2023/06/22 00:00:21 INFO mlflow.tracking._model_registry.client: Waiting up to 300 seconds for model version to finish creation. Model name: ElasticnetWineModel, version 1
+Created version '1' of model 'ElasticnetWineModel'.
+```
+
+
+Run mlflow ui repository root to loads the corresponding runs in `runs` folder for viewing in tracking ui.
+
+
+```sh
+export RUNS=<path-to-runs-folder>
+poetry run mlflow ui --backend-store-uri $RUNS \
+          --default-artifact-root $RUNS \
+          --host 0.0.0.0 \
+          --port 5000
+```
+
+You should see the server start
+
+```bash
+[2023-06-22 00:05:44 +0100] [60920] [INFO] Starting gunicorn 20.1.0
+[2023-06-22 00:05:44 +0100] [60920] [INFO] Listening at: http://0.0.0.0:5000 (60920)
+[2023-06-22 00:05:44 +0100] [60920] [INFO] Using worker: sync
+[2023-06-22 00:05:44 +0100] [60925] [INFO] Booting worker with pid: 60925
+[2023-06-22 00:05:44 +0100] [60926] [INFO] Booting worker with pid: 60926
+[2023-06-22 00:05:44 +0100] [60927] [INFO] Booting worker with pid: 60927
+[2023-06-22 00:05:44 +0100] [60928] [INFO] Booting worker with pid: 60928
+```
+
+![](images/ui-table.png)
+![](images/ui-metrics-diff-runs.png)
--- mlflow_with_dvc/pipeline.py
+++ mlflow_with_dvc/pipeline.py
@@ -1,26 +1,17 @@
 import logging
 import sys
 import warnings
-from urllib.parse import urlparse
-
 import mlflow.sklearn
 import numpy as np
-import pandas as pd
+from urllib.parse import urlparse
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_wine
+from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Get URL from dvc
-import dvc.api
-
-path = "data/wine-quality.csv"
-repo = "https://drive.google.com/drive/folders/154SWBs2KMtoX3Qd2jA7cPUXNTM9X7wEg"
-version = "v1"
-
-data_url = dvc.api.get_url(path=path, repo=repo, rev=version)
 
 
 def eval_metrics(actual, pred):
@@ -33,28 +24,25 @@ def eval_metrics(actual, pred):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
-
-    # read the wine-qualtiy csv file from remote repository
-    data = pd.read_csv(data_url, sep=";")
-
+    data = load_wine(as_frame=True)
+    df = data['frame']
     # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
+    train, test = train_test_split(df)
 
     # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
+    train_x = train.drop(["target"], axis=1)
+    test_x = test.drop(["target"], axis=1)
+    train_y = train[["target"]]
+    test_y = test[["target"]]
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
+    mlflow.set_tracking_uri(str(Path(__file__).parent.parent / 'runs'))
     with mlflow.start_run():
         # Log data params
-        mlflow.log_param("data_url", data_url)
-        mlflow.log_param("data_version", version)
-        mlflow.log_param("input_rows", data.shape[0])
-        mlflow.log_param("input_columns", data.shape[1])
+        mlflow.log_param("data_version", 'v1')
+        mlflow.log_param("input_rows", df.shape[0])
+        mlflow.log_param("input_columns", df.shape[1])
 
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
@@ -63,10 +51,10 @@ if __name__ == "__main__":
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print(f"Elasticnet model (alpha={alpha:f}, l1_ratio={l1_ratio:f}):")
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+        logger.info(f"Elasticnet model (alpha={alpha:f}, l1_ratio={l1_ratio:f}):")
+        logger.info(f"RMSE: {rmse}")
+        logger.info(f"MAE: {mae}")
+        logger.info(f"R2: {r2}")
 
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
@@ -79,9 +67,6 @@ if __name__ == "__main__":
         # Model registry does not work with file store
         if tracking_url_type_store != "file":
             # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
                 lr, "model", registered_model_name="ElasticnetWineModel"
             )
